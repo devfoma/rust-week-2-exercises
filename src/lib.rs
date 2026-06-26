4365995150490de46aa6558b3fdb9e5157878002
devfoma <faithamarachi860@gmail.com>
2026-06-26 04:44:29 +0100

    more exercises

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,16 @@
 use hex::{decode, encode};
 
-// pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
-//     // TODO: Decode hex string into Vec<u8>, return error string on failure
-
-// }
+pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
+    // TODO: Decode hex string into Vec<u8>, return error string on failure
+    decode(hex_str).map_err(|e| e.to_string())
+}
 
 // pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
 //     // TODO: Reverse the byte order of input slice and return as Vec<u8>
 // }
 
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
-    // TODO: Implement conversion of bytes slice to hex string
-    let bytes: &[u8] = bytes;
-
-    let hex_string = hex::encode(bytes);
-
-    println!("Hex string: {}", hex_string);
-
-    hex_string
+    encode(bytes)
 }
 
 // pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
